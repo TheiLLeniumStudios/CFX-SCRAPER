4c74a60993ecda3e70565fbc88353646e3c6e071
Kiyomi <54733885+K3YOMI@users.noreply.github.com>
2022-08-31 12:57:38 -0500

    v2 - Fixed Requests

--- CFX.re-Scraper/Javascript/_install.py
+++ CFX.re-Scraper/Javascript/_install.py
@@ -6,7 +6,7 @@ def log(text):
     print("[" + _getCurrentTime() + "] " + text)
 def _installNodeJSPackages():
     log("Installing [Request] Package")
-    os.system('npm i request --prefix ./Javascript') # Install request package in the specified directory [./Javascript]
+    os.system('npm i requests --prefix ./Javascript') # Install request package in the specified directory [./Javascript]
     log("Finished Installing [Request] Package")
     log("Finished Install Process, you may now use the tool")
     exit()
