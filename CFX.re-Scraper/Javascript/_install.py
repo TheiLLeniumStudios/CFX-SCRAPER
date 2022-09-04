e42f7fa3e0935ec196e21e9bf223ef0636afc8a8
Kiyomi <54733885+K3YOMI@users.noreply.github.com>
2022-09-04 07:14:15 -0500

    v2 - NPM Request will still install but a warn message will appear / Ignore it

--- CFX.re-Scraper/Javascript/_install.py
+++ CFX.re-Scraper/Javascript/_install.py
@@ -6,7 +6,9 @@ def log(text):
     print("[" + _getCurrentTime() + "] " + text)
 def _installNodeJSPackages():
     log("Installing [Request] Package")
-    os.system('npm i requests --prefix ./Javascript') # Install request package in the specified directory [./Javascript]
+    os.system('npm i request --prefix ./Javascript')
+    os.system('cls || clear') # Clear the terminal
+    log("Ignore the [npm WARN] messages. They are not important.")
     log("Finished Installing [Request] Package")
     log("Finished Install Process, you may now use the tool")
     exit()
